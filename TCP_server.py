b10c98e621182abebfe1db1a82856233b085f756
zRainerzz <ghaithkhedhri3.com>
2025-01-16 15:10:01 +0100

    ss

--- TCP_server.py
+++ TCP_server.py
@@ -15,12 +15,12 @@ print ('[*] Listening on %s:%d' %(bind_ip, bind_port))
 #This is our client-handling thread.
 def handle_client(client_socket):
     
-    #Print out what the client sends
+    #Print out what the client sends.
     request = client_socket.recv(1024)
     
     print('[*] Received %s' % request)
     
-    #Send back packet
+    #Send back packet.
     client_socket.send("ACK!")
     
     client_socket.close()
@@ -31,4 +31,5 @@ while True:
     
     print('[*] Accepted Connection from %s:%d' %(addr[0],addr[1]))
     
-    #spin up our client thread
+    #Spin up our client thread to handle incoming data.
+    
